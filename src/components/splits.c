f6bf35814c8de110f080a7f0f1abd13060e8031c
Dark <dark@volatile.bz>
2019-10-18 16:06:36 -0400

    document src/components/splits.c
    This code is an absolute mess and should really be rewritten.
    
    For now, I'm just going to document it so its a bit easier to understand
    what the hell is actually going on there.

--- src/components/splits.c
+++ src/components/splits.c
@@ -7,7 +7,9 @@
 #include "darksplit.h"
 #include "color.h"
 
-//FIXME: make this less shitty
+// FIXME: make this less shitty
+// But no seriously this code is awful and I should consider rewriting it to
+// something cleaner.
 void render_splits(json_t *data)
 {
 	json_t *splits = json_object_get(data, "splits");
@@ -16,6 +18,8 @@ void render_splits(json_t *data)
 	size_t i, j;
 	const char *str;
 	int colwidth[16] = {0};
+	//We need to loop through the splits array in 2 passes here.
+	//This first pass is used to calculate the width of the columns.
 	json_array_foreach(splits, i, split) {
 		columns = json_object_get(split, "columns");
 		json_array_foreach(columns, j, column) {
@@ -25,27 +29,35 @@ void render_splits(json_t *data)
 		}
 	}
 
+	//On the second pass, we will actually draw the splits.
 	int y, x;
 	getyx(stdscr, y, x);
 	json_array_foreach(splits, i, split) {
+		//if this is the current split, use reverse video.
 		if (json_obj_bool(split, "is_current_split"))
 			attron(A_REVERSE);
 
+		//get the name of the split and draw it.
 		str = json_obj_string(split, "name");
 		mvprintw(y, 0, "%-*.*s", WIDTH, WIDTH, str);
 
+		//this loop will draw the columns
 		x = WIDTH;
 		columns = json_object_get(split, "columns");
 		json_array_foreach(columns, j, column) {
+			//move the cursor back the width of the current column
 			x -= colwidth[j] + 1;
 			str = json_obj_string(column, "value");
 
+			//get the semantic color for the column
 			int color = get_semantic_color(json_obj_string(
 				column,"semantic_color"));
+			//draw the column
 			attron(color);
 			mvprintw(y, x, "%*s", colwidth[j] + 1, str);
 			attroff(color);
 		}
+		//advance to the next line and disable reverse video
 		move(++y, 0);
 		attroff(A_REVERSE);
 	}
