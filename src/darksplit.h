e1bd109483ce78a86a8dbe836e7f8a34734811dd
Dark <dark@volatile.bz>
2019-11-30 01:27:00 -0500

    remove jansson macros
    forgot to remove these

--- src/darksplit.h
+++ src/darksplit.h
@@ -2,11 +2,6 @@
 
 #include <livesplit_core.h>
 
-#define json_obj_string(obj, key)                                              \
-	json_string_value(json_object_get((obj), (key)))
-#define json_obj_bool(obj, key)                                                \
-	json_boolean_value(json_object_get((obj), (key)))
-
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
